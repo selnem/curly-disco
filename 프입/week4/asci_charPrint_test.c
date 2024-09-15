9b0eb5e9510ef5ef212ebdb4101e4e14ce6c60cd
selnem <tjdghks0130@naver.com>
2024-09-15 16:16:49 +0900

    file name err fix

--- 프입/week4/asci_charPrint_test.c
+++ 프입/week4/asci_charPrint_test.c
@@ -3,7 +3,7 @@ int main(){
     printf("print char\n");
     
     char ch_arr[]={'N',73,'d','m','K','-',111,71,'\n'};
-    int ch_order[10]={5,1,0,7,5,2,6,3,8};
+    int ch_order[10]={4,1,0,7,5,2,6,3,8};
     for(int i=0;i<8;i++){
         putchar(ch_arr[ch_order[i]]);
     }
