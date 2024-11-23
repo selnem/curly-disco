680d0cb885bf90c6a8a7100f3e0215c9f5e849d0
selnem <tjdghks0130@naver.com>
2024-11-23 22:36:11 +0900

    dd

--- pep/PJ#2/Q_1.c
+++ pep/PJ#2/Q_1.c
@@ -25,7 +25,7 @@ void print_student(const STUDENT *s){
 void print_student_list(STUDENT *student_arr,int numOfstudent){
     printf("\n%8s  %-11s %-5s %10s %14s\n", "번호", "이름", "학번", "학기", "수강과목");
     for(int i=0;i<numOfstudent-1;i++){
-        for(int j=0;j<numOfstudent-i;j++){
+        for(int j=0;j<numOfstudent-i-1;j++){
             if(student_arr[j].semester>student_arr[j+1].semester){
                 s_list_align[0]=student_arr[j];
                 student_arr[j]=student_arr[j+1];
@@ -43,6 +43,34 @@ void print_student_list(STUDENT *student_arr,int numOfstudent){
 // 학생 번호: printf("%3d번: ",변수); 이용하여 출력
 // 학생 정보: print_student 함수 이용하여 출력 
 /* -------------------------- */
+int find_student(STUDENT *list,int size){
+    char wordOfserch[BUF_SIZE];
+    scanf("%s",wordOfserch);
+    int find_bool=2;//0:exit,1:find,2:can't find
+    if(strcmp("exit",wordOfserch)==0){
+        find_bool=0;
+        return find_bool;
+    }
+    for(int i=0;i<size;i++){
+        int k=0;
+        if(strcmp(list[i].name,wordOfserch)==0){
+            find_bool=1;
+            k=1;
+        }
+        else if(strcmp(list[i].class_name,wordOfserch)==0){
+            find_bool=1;
+            k=1;
+        }
+        if(k==1){
+            printf("%3d번: ",i+1);
+            print_student(&list[i]);
+        }
+    }
+    if(find_bool==2){
+        printf("해당 학생 정보를 찾을 수 없습니다.");
+    }
+    return find_bool;
+}
 
 /* find_student 함수 구현 */
 // 반드시 다음 코드 사용해서 출력하세요.
@@ -51,6 +79,14 @@ void print_student_list(STUDENT *student_arr,int numOfstudent){
 /* -------------------------- */
 
 /* print_presentation_list 함수 구현 */
+void print_presentation_list(const STUDENT *stu_list,int *pr_list ,int cnt){
+    printf("<< 발표리스트 >>\n");
+    for(int i=0;i<cnt;i++){
+        printf("%3d번: ",i+1);
+        print_student(&stu_list[pr_list[i]]);
+    }
+    printf("총 발표명 수: %d명",cnt);
+}
 // 반드시 다음 코드 사용해서 출력하세요.
 // 가장 윗줄: printf("<< 발표리스트 >>\n"); 이용하여 출력
 // 학생 번호: printf("%3d번: ",변수); 이용하여 출력
@@ -77,6 +113,7 @@ int main(void)
     // printf("\n찾으려는 학생의 이름 또는 수강과목을 입력하세요(종료는 exit): ");
     
     // #1-3 구현 시 반드시 다음 코드 사용해서 출력하세요.
+    
     // printf("\n발표리스트에 추가하려면 1번, 삭제하려면 2번, 종료하려면 3번을 누르세요: ");
     // printf("발표리스트에 추가할 학생 번호를 입력하세요: ");
     // printf("이미 발표리스트에 있는 학생입니다.\n");
@@ -85,7 +122,63 @@ int main(void)
     // printf("삭제할 학생이 없습니다.\n");
     
     /* Put your answer */
-    int studentNUM=sizeof(s_list)/sizeof(STUDENT);
-    print_student_list(s_list,studentNUM);
+    int size=sizeof(s_list)/sizeof(STUDENT);//학생수
+    //#1-1
+    print_student_list(s_list,size);
+
+    //#1-2
+    
+    do{
+        printf("\n찾으려는 학생의 이름 또는 수강과목을 입력하세요(종료는 exit): ");
+
+    }while(find_student(s_list,size));
+
+    //#1-3
+    int mode,st_num,i;
+    int cnt=0;
+    int pr_StuList[MAX_PRESENTATION_LIST]={0};
+        do{
+            printf("\n발표리스트에 추가하려면 1번, 삭제하려면 2번, 종료하려면 3번을 누르세요: ");
+            scanf("%d",&mode);
+            switch (mode){
+                case 1:
+                    printf("발표리스트에 추가할 학생 번호를 입력하세요: ");
+                    scanf("%d",&st_num);
+                    if(!(0<st_num&&st_num<=size)){
+                        printf("잘못된 번호입니다.\n");
+                    }
+                    else{
+                        for(i=0;i<cnt;i++){
+                            if(pr_StuList[i]==st_num){
+                                printf("이미 발표리스트에 있는 학생입니다.\n");
+                                break;
+                            }
+                        }
+                        if(i==cnt){
+                            pr_StuList[cnt++]=st_num-1;
+                            print_presentation_list(s_list,pr_StuList,cnt);
+                        }
+                    }
+                    break;
+                case 2:
+                    printf("발표리스트에서 삭제할 학생 번호를 입력하세요: ");
+                    scanf("%d",&st_num);
+                    if(st_num<1||st_num>cnt){
+                        printf("삭제할 학생이 없습니다.\n");
+                    }
+                    else{
+                       for(int t=st_num-1;t<cnt-1;t++){
+                            pr_StuList[t]=pr_StuList[t+1];
+                       }
+                       cnt--;
+                       print_presentation_list(s_list,pr_StuList,cnt);
+                    }
+                case 3:
+                    break;
+                default:
+                    printf("모드를 재입력하세요");
+                    break;
+            }
+        }while(mode!=3&&cnt<3);
     return 0;
 }
---
+++ pep/PJ#2/Q_2.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <string.h>
+
+/* 변수 선언 부분: 수정하지 마세요 */
+#define price_americano 3500
+#define price_icetea 3000
+#define price_smoothie 5000
+#define price_carrot 7000
+#define price_cheese 6000
+#define price_choco 5500
+/* ------------------------------ */
+
+int main(){
+    /* 변수 선언 부분: 수정하지 마세요 */
+    int num_americano;
+    int num_icetea;
+    int num_smoothie;
+    int num_carrot;
+    int num_cheese;
+    int num_choco;
+    int num_set1;
+    int num_set2;
+    int total_price_set1;
+    int total_price_set2;
+    int total_price;
+    /* ------------------------------ */
+
+    // 반드시 다음 코드 사용해서 출력하세요. 
+    // 주문 받을 때: printf("주문하고자 하는 음료(커피, 아이스티, 스무디)와 케잌(당근, 치즈, 초코)의 갯수를 각각 입력하세요:\n"); 이용하여 출력
+    // 음료 갯수가 10개 넘었을 때: printf("음료 갯수가 10개를 넘었습니다. 다시 주문해주세요.\n\n"); 이용하여 출력
+    // 케잌 갯수가 10개 넘었을 때: printf("케잌 갯수가 10개를 넘었습니다. 다시 주문해주세요.\n\n"); 이용하여 출력
+    
+    /*put your answer*/
+    while(1){
+        
+        printf("주문하고자 하는 음료(커피, 아이스티, 스무디)와 케잌(당근, 치즈, 초코)의 갯수를 각각 입력하세요:\n");
+        scanf("%d %d %d %d %d %d",&num_americano,&num_icetea,&num_smoothie,&num_carrot,&num_cheese,&num_choco);
+        
+        if((num_americano+num_icetea+num_smoothie)>10){
+            printf("음료 갯수가 10개를 넘었습니다. 다시 주문해주세요.\n\n");
+            continue;
+        }
+        else if(num_carrot+num_cheese+num_choco>10){
+            printf("케잌 갯수가 10개를 넘었습니다. 다시 주문해주세요.\n\n");
+            continue;
+        }
+        else{
+            break;
+        }        
+    }
+    if()
+    /* 출력 부분: 수정하지 마세요 */ 
+    printf("\n품목      갯수   금액\n");
+    if (num_set1 > 0)
+        printf("세트1    %4d %7d\n",num_set1, total_price_set1);
+    if (num_set2 > 0)
+        printf("세트2    %4d %7d\n", num_set2, total_price_set2);
+    if (num_americano > 0)
+        printf("커피     %4d %7d\n", num_americano, price_americano * num_americano);
+    if (num_icetea > 0)
+        printf("아이스티 %4d %7d\n", num_icetea, price_icetea * num_icetea);
+    if (num_smoothie > 0)
+        printf("스무디   %4d %7d\n", num_smoothie, price_smoothie * num_smoothie);
+    if (num_carrot > 0)
+        printf("당근케잌 %4d %7d\n", num_carrot, price_carrot * num_carrot);
+    if (num_cheese > 0)
+        printf("치즈케잌 %4d %7d\n", num_cheese, price_cheese * num_cheese);
+    if (num_choco > 0)
+        printf("초코케잌 %4d %7d\n", num_choco, price_choco * num_choco);
+    printf("----------------------------\n");
+    printf("총 지불 금액 %8d\n", total_price);
+    /* ------------------------- */
+
+    return 0;
+
+}
