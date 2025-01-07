1ddb243524263980728ace62078cb4e951cc722e
selnem <tjdghks0130@naver.com>
2025-01-08 01:11:48 +0900

    ㅁㄴㅇㄹ

---
+++ koala/week1/3053.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class Circle{
+    private:
+        double pi,Uarea,Tarea;
+        int R;
+
+
+    public:
+        Circle(int R):pi(3.14159265359)
+        {
+            Uarea=pi*R*R;
+            Tarea=2*R*R;
+        }
+        double getUarea();
+        double getTarea();
+
+};
+double Circle::getUarea(){
+    return Uarea;
+}
+
+double Circle::getTarea(){
+    return Tarea;
+}
+
+int main(){
+    int R;
+    cin>>R;
+    Circle C=R;
+    cout<<""
+
+}
